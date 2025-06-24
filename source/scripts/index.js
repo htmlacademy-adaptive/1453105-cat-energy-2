787dd0f774a1f68294221da2bda67ffb5d29263f
Daria Petrova <63007351+Daria-Petrova@users.noreply.github.com>
2025-06-24 19:43:51 +0300

    Разделы 5 и 6 (#10)
    * Добавлены иконки
    
    * Черновик header
    
    * Черновик toggle
    
    * Сделан header для мобильной версии
    
    * Сделан example для мобильной версии
    
    * Черновик футера
    
    * footer + example
    
    * Сделан 5 раздел
    
    * Сделан 6 раздел частично
    
    * Сделано таблет версия
    
    * 6 раздел
    
    * правки линта
    
    * правки js
    
    * Ещё одна версия скипта
    
    ---------
    
    Co-authored-by: Дарья Петрова <daria.petrova@yoso.ru>

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,1 +1,7 @@
 /* в этот файл добавляет скрипты*/
+document.getElementById('toggle-button').addEventListener('click', () => {
+  const btn = document.getElementById('toggle-button');
+  const menu = document.getElementById('toggle-menu');
+  btn.classList.toggle('toggle--open');
+  menu.classList.toggle('navigation-list--closed');
+});
