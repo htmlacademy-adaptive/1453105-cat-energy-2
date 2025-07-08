0cacb02ea8362853c431d40cebb553ad69a338e2
Daria Petrova <63007351+Daria-Petrova@users.noreply.github.com>
2025-07-08 19:44:13 +0300

    Module7 task1 (#12)
    * Сделан слайдер
    
    * Сделана карта
    
    * Правки котика
    
    * Сделан центральный котик
    
    * Сделаны декоративные полосочки
    
    * Добавлено отображение навигации при отключенном js
    
    * правка котика
    
    * правка линта
    
    * Поправлен header без js, переделан пример
    
    * временный коммит
    
    * Промежуточный коммит
    
    * правки каталога
    
    * правки каталога
    
    * правки каталога
    
    * правки линт
    
    ---------
    
    Co-authored-by: Дарья Петрова <daria.petrova@yoso.ru>

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,6 +1,10 @@
 /* в этот файл добавляет скрипты*/
-document.getElementById('toggle-button').addEventListener('click', () => {
-  const btn = document.getElementById('toggle-button');
+window.addEventListener('DOMContentLoaded', () => {
+  document.querySelector('.header__navigation').classList.remove('header__navigation--none-js');
+});
+
+document.querySelector('.toggle__button').addEventListener('click', () => {
+  const btn = document.querySelector('.toggle__button');
   const menu = document.getElementById('toggle-menu');
   btn.classList.toggle('toggle--open');
   menu.classList.toggle('navigation-list--closed');
